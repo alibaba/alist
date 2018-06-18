698f5b26af8776301cf0ee160cef664b324be084
quirkyshop <quirkyvar@gmail.com>
2018-06-18 23:32:40 +0800

    feat: umd

--- builder/umd.js
+++ builder/umd.js
@@ -1,18 +1,18 @@
-'use strict'
+
 
 // 设置 ENV
-const ENV_TYPE = process.env.NODE_ENV || 'production'
+const ENV_TYPE = process.env.NODE_ENV || 'production';
 // const ENV_TYPE = process.env.NODE_ENV || 'development'
-process.env.NODE_ENV = ENV_TYPE
-process.env.BABEL_ENV = ENV_TYPE
+process.env.NODE_ENV = ENV_TYPE;
+process.env.BABEL_ENV = ENV_TYPE;
 
 // 保证 unhandle process 能抛出异常
-process.on('unhandledRejection', err => {
-  throw err
-})
+process.on('unhandledRejection', (err) => {
+    throw err;
+});
 
 // 加载环境变量
-require('./lib/env')
+require('./lib/env');
 
 const sass = require('node-sass');
 const paths = require('./lib/paths');
@@ -25,49 +25,50 @@ const { rmdirSync, mkdir } = require('./utils');
 const umdConfig = require('./lib/webpack.umd');
 
 const transformCss = async () => {
-  const CSS_PATTERN = /\.(scss)$/
-  let files = glob.sync('**/*.*', {
-    cwd: paths.appSrc
-  })
+    const CSS_PATTERN = /\.(scss)$/;
+    const files = glob.sync('**/*.*', {
+        cwd: paths.appSrc,
+    });
 
-  for (let file of files) {
-    let srcFile = path.join(paths.appSrc, file)
-    let destFile = path.join(paths.appBuild, file)
+    for (const file of files) {
+        const srcFile = path.join(paths.appSrc, file);
+        let destFile = path.join(paths.appBuild, file);
 
-    if (file.match(CSS_PATTERN)) {
-      console.log(' - %s %s', chalk.green('scss'), file)
-      destFile = destFile.replace(CSS_PATTERN, '.css')
-      let content = fs.readFileSync(srcFile, 'utf8')
-      await mkdir(path.dirname(destFile))
-      const rawScss = sass.renderSync({
-        data: content
-      });
+        if (file.match(CSS_PATTERN)) {
+            destFile = destFile.replace(CSS_PATTERN, '.css');
+            console.log(' - %s %s', chalk.green('scss'), file);
+            const content = fs.readFileSync(srcFile, 'utf8');
+            await mkdir(path.dirname(destFile));
+            const rawScss = sass.renderSync({
+                data: content,
+                file: srcFile,
+            });
 
-      const cssContent = rawScss.css.toString('utf-8');
-      fs.writeFileSync(destFile, cssContent);
+            const cssContent = rawScss.css.toString('utf-8');
+            fs.writeFileSync(destFile, cssContent);
+        }
     }
-  }
-  console.log(chalk.green('\nTransform successfully!'))
+    console.log(chalk.green('\nTransform successfully!'));
 };
 
 const callback = async (err, stats) => {
-  if (err) {
-    console.error(err.stack || err)
-    if (err.details) console.error(err.details)
-    return;
-  }
+    if (err) {
+        console.error(err.stack || err);
+        if (err.details) console.error(err.details);
+        return;
+    }
 
-  console.log(stats.toString({
-    chunks: false, // Makes the build much quieter
-    chunkModules: false,
-    colors: true, // Shows colors in the console
-    children: false,
-    builtAt: true,
-    modules: false
-  }))
+    console.log(stats.toString({
+        chunks: false, // Makes the build much quieter
+        chunkModules: false,
+        colors: true, // Shows colors in the console
+        children: false,
+        builtAt: true,
+        modules: false,
+    }));
 
-  await transformCss();
-}
+    await transformCss();
+};
 
 rmdirSync(paths.appBuild);
 webpack(umdConfig).run(callback);
