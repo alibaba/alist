d0d41400c89270bcddede8485aa38c8c61d804bd
guishu.zc <guishu.zc@alibaba-inc.com>
2018-07-23 13:59:11 +0800

    feat: update lib

--- builder/index.js
+++ builder/index.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-const path = require('path')
-const fs = require('fs')
+const path = require('path');
+const fs = require('fs');
 
-const scriptsDir = path.join(__dirname)
-const command = process.argv[2] // Enums: [dev, test, build]
+const scriptsDir = path.join(__dirname);
+const command = process.argv[2]; // Enums: [dev, test, build]
 const commands = fs.readdirSync(scriptsDir)
-  .filter(name => name.endsWith('.js'))
-  .map(name => path.basename(name, '.js'))
+    .filter(name => name.endsWith('.js'))
+    .map(name => path.basename(name, '.js'));
 
 if (commands.indexOf(command) < 0) {
-  console.log('Usage: npm run <%s>', commands.join('|'))
-  process.exit()
+    console.log('Usage: npm run <%s>', commands.join('|'));
+    process.exit();
 }
 
 // run script
-require(path.join(scriptsDir, command + '.js'))
+require(path.join(scriptsDir, `${command}.js`));
--- builder/lib/webpack.umd.js
+++ builder/lib/webpack.umd.js
@@ -1,49 +1,43 @@
-const path = require('path');
-
-const root = process.cwd();
-const dist = path.join(root, 'dist');
 const entryOuts = require('../entryOuts');
 const babelLoaderOpts = require('../lib/babelConfig');
 
 const mode = 'production';
 const basicConfig = {
-    mode: mode,
+    mode,
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /(node_modules|bower_components)/,
                 use: {
-                loader: 'babel-loader',
-                options: babelLoaderOpts
-                }
-            }
-        ]
+                    loader: 'babel-loader',
+                    options: babelLoaderOpts,
+                },
+            },
+        ],
     },
     externals: {
-        "react": {
+        react: {
             root: 'React',
             commonjs2: 'react',
             commonjs: 'react',
-            amd: 'react'
+            amd: 'react',
         },
-        "react-dom": {
+        'react-dom': {
             root: 'ReactDOM',
             commonjs2: 'react-dom',
             commonjs: 'react-dom',
-            amd: 'react-dom'
-        }
+            amd: 'react-dom',
+        },
     },
     resolve: {
         extensions: ['.js', '.jsx'],
-    }
+    },
 };
 
-const config = entryOuts.map((entryOutItem) => {
-    return {
-        ...entryOutItem,
-        ...basicConfig
-    };
-});
+const config = entryOuts.map(entryOutItem => ({
+    ...entryOutItem,
+    ...basicConfig,
+}));
 
 module.exports = config;
