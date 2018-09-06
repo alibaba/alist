424fa0a2e86a5aac53f1aa70f58e0e640442bf9e
quirkyshop <qurikyvar@gmail.com>
2018-09-06 19:49:39 +0800

    feat: add repeater entry

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Item from './component/Item';
 import If from './component/If';
 import FormCore from './core/form';
 import RepeaterCore from './repeater/repeaterCore';
+import Repeater from './repeater';
 
 export {
     Form as default,
@@ -12,4 +13,5 @@ export {
     If,
     FormCore,
     RepeaterCore,
+    Repeater,
 };
