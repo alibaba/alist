199f0c0f97620eabc6e6fdcd82a7ea5d20b05bae
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-07 02:22:37 +0000

    Deploy alibaba/nopage to github.com/alibaba/nopage.git:gh-pages

--- precache-manifest.58d537a8a8faa18e4624528426c288b6.js
+++ precache-manifest.58d537a8a8faa18e4624528426c288b6.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.4a686d48.js"
   },
   {
-    "revision": "b6a13c284b10e05b3a52",
-    "url": "./static/js/main.b6a13c28.chunk.js"
+    "revision": "cdf146cc8fc6f5d4b9c9",
+    "url": "./static/js/main.cdf146cc.chunk.js"
   },
   {
     "revision": "fec2613dbcf50ab43daf",
     "url": "./static/js/1.fec2613d.chunk.js"
   },
   {
-    "revision": "b6a13c284b10e05b3a52",
+    "revision": "cdf146cc8fc6f5d4b9c9",
     "url": "./static/css/main.50c40711.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "./static/css/1.27df7d0e.chunk.css"
   },
   {
-    "revision": "fb6d4a453f5e9706bd41bc283a2e0be3",
+    "revision": "f55a186620e33842c9688e8cde5f8da5",
     "url": "./index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.58d05c5621ff4496ef15a21359fcdea5.js"
+  "./precache-manifest.58d537a8a8faa18e4624528426c288b6.js"
 );
 
 workbox.clientsClaim();
