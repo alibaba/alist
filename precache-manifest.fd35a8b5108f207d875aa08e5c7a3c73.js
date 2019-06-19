7c8119da479535aab1eba6347b7ce5b761394dba
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-19 15:30:53 +0000

    Deploy alibaba/nopage to github.com/alibaba/nopage.git:gh-pages

---
+++ precache-manifest.fd35a8b5108f207d875aa08e5c7a3c73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a3a0978834655eadc804efda1d3a443",
+    "url": "./static/media/github-icon.0a3a0978.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "7d93372cb2a8672cc702",
+    "url": "./static/js/main.7d93372c.chunk.js"
+  },
+  {
+    "revision": "fec2613dbcf50ab43daf",
+    "url": "./static/js/1.fec2613d.chunk.js"
+  },
+  {
+    "revision": "7d93372cb2a8672cc702",
+    "url": "./static/css/main.50c40711.chunk.css"
+  },
+  {
+    "revision": "fec2613dbcf50ab43daf",
+    "url": "./static/css/1.27df7d0e.chunk.css"
+  },
+  {
+    "revision": "609f60c70aee0de43f1346043182e1bd",
+    "url": "./index.html"
+  }
+];
