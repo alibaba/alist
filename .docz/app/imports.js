da49e78984a808b962c3cb26148b297b56094732
guishu.zc <guishu.zc@alibaba-inc.com>
2019-06-24 22:00:04 +0800

    feat: add static

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,8 +1,4 @@
 export const imports = {
-  'src/mdx/component/form-item.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-mdx-component-form-item" */ 'src/mdx/component/form-item.mdx'
-    ),
   'src/mdx/component/dialog.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-mdx-component-dialog" */ 'src/mdx/component/dialog.mdx'
@@ -11,29 +7,25 @@ export const imports = {
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-mdx-component-if" */ 'src/mdx/component/if.mdx'
     ),
-  'src/mdx/component/form.mdx': () =>
+  'src/mdx/component/form-item.mdx': () =>
     import(
-      /* webpackPrefetch: true, webpackChunkName: "src-mdx-component-form" */ 'src/mdx/component/form.mdx'
+      /* webpackPrefetch: true, webpackChunkName: "src-mdx-component-form-item" */ 'src/mdx/component/form-item.mdx'
     ),
   'src/mdx/component/standard.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-mdx-component-standard" */ 'src/mdx/component/standard.mdx'
     ),
-  'src/mdx/list/index.mdx': () =>
+  'src/mdx/component/form.mdx': () =>
     import(
-      /* webpackPrefetch: true, webpackChunkName: "src-mdx-list-index" */ 'src/mdx/list/index.mdx'
-    ),
-  'src/mdx/list/multiple.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-mdx-list-multiple" */ 'src/mdx/list/multiple.mdx'
-    ),
-  'src/mdx/list/query.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-mdx-list-query" */ 'src/mdx/list/query.mdx'
+      /* webpackPrefetch: true, webpackChunkName: "src-mdx-component-form" */ 'src/mdx/component/form.mdx'
     ),
   'src/mdx/core/index.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-mdx-core-index" */ 'src/mdx/core/index.mdx'
+    ),
+  'src/mdx/core/interceptor.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-mdx-core-interceptor" */ 'src/mdx/core/interceptor.mdx'
     ),
   'src/mdx/core/props.mdx': () =>
     import(
@@ -43,10 +35,6 @@ export const imports = {
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-mdx-core-reset" */ 'src/mdx/core/reset.mdx'
     ),
-  'src/mdx/core/interceptor.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-mdx-core-interceptor" */ 'src/mdx/core/interceptor.mdx'
-    ),
   'src/mdx/core/status.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-mdx-core-status" */ 'src/mdx/core/status.mdx'
@@ -54,6 +42,10 @@ export const imports = {
   'src/mdx/core/validate-inner.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-mdx-core-validate-inner" */ 'src/mdx/core/validate-inner.mdx'
+    ),
+  'src/mdx/getStart/antd-pro.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-mdx-get-start-antd-pro" */ 'src/mdx/getStart/antd-pro.mdx'
     ),
   'src/mdx/core/validate.mdx': () =>
     import(
@@ -63,14 +55,6 @@ export const imports = {
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-mdx-core-value" */ 'src/mdx/core/value.mdx'
     ),
-  'src/mdx/getStart/antd-pro.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-mdx-get-start-antd-pro" */ 'src/mdx/getStart/antd-pro.mdx'
-    ),
-  'src/mdx/getStart/antd.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-mdx-get-start-antd" */ 'src/mdx/getStart/antd.mdx'
-    ),
   'src/mdx/getStart/fusion.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-mdx-get-start-fusion" */ 'src/mdx/getStart/fusion.mdx'
@@ -79,13 +63,33 @@ export const imports = {
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-mdx-get-start-overview" */ 'src/mdx/getStart/overview.mdx'
     ),
+  'src/mdx/list/multiple.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-mdx-list-multiple" */ 'src/mdx/list/multiple.mdx'
+    ),
+  'src/mdx/list/index.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-mdx-list-index" */ 'src/mdx/list/index.mdx'
+    ),
+  'src/mdx/getStart/antd.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-mdx-get-start-antd" */ 'src/mdx/getStart/antd.mdx'
+    ),
   'src/mdx/getStart/usage.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-mdx-get-start-usage" */ 'src/mdx/getStart/usage.mdx'
     ),
+  'src/mdx/list/query.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-mdx-list-query" */ 'src/mdx/list/query.mdx'
+    ),
   'src/mdx/repeater/async-handler.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-mdx-repeater-async-handler" */ 'src/mdx/repeater/async-handler.mdx'
+    ),
+  'src/mdx/repeater/basic.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-mdx-repeater-basic" */ 'src/mdx/repeater/basic.mdx'
     ),
   'src/mdx/repeater/config.mdx': () =>
     import(
@@ -98,9 +102,5 @@ export const imports = {
   'src/mdx/repeater/view.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-mdx-repeater-view" */ 'src/mdx/repeater/view.mdx'
-    ),
-  'src/mdx/repeater/basic.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-mdx-repeater-basic" */ 'src/mdx/repeater/basic.mdx'
     ),
 }
