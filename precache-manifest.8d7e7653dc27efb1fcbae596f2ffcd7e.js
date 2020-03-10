f384bc47f4f58358eed69cd6c206ebcadfaefcc1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-10 04:56:17 +0000

    Deploy alibaba/alist to github.com/alibaba/alist.git:gh-pages

---
+++ precache-manifest.8d7e7653dc27efb1fcbae596f2ffcd7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a3a0978834655eadc804efda1d3a443",
+    "url": "./static/media/github-icon.0a3a0978.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4ed5bbace7d10bc58792",
+    "url": "./static/js/main.4ed5bbac.chunk.js"
+  },
+  {
+    "revision": "09deb064ab57744f8a18",
+    "url": "./static/js/1.09deb064.chunk.js"
+  },
+  {
+    "revision": "4ed5bbace7d10bc58792",
+    "url": "./static/css/main.d05b68b5.chunk.css"
+  },
+  {
+    "revision": "09deb064ab57744f8a18",
+    "url": "./static/css/1.a7cc1053.chunk.css"
+  },
+  {
+    "revision": "762bc7777758afbf26e5178b83dd510b",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6817b1064fa981b9e5e04d471f4bdeed.js"
+  "./precache-manifest.8d7e7653dc27efb1fcbae596f2ffcd7e.js"
 );
 
 workbox.clientsClaim();
