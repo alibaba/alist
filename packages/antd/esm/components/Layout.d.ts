9af80bd70392414d9c0377c6b60a79fc0911c3df
quirkyshop <quirkyvar@gmail.com>
2020-05-08 13:52:37 +0800

    feat: add layout types

--- packages/antd/esm/components/Layout.d.ts
+++ packages/antd/esm/components/Layout.d.ts
@@ -1,4 +1,19 @@
-declare let Layout: {
+import React from 'react';
+declare let Layout: React.FC<{
+    name?: string;
+    visible?: boolean;
+    display?: boolean;
+    gap?: number[];
+    columns?: number;
+    full?: boolean;
+    label?: any;
+    suffix?: any;
+    labelAlign?: string;
+    labelCol?: any;
+    wrapperCol?: any;
+    labelWidth?: any;
+    mode?: string;
+}> & {
     (): any;
     ButtonGroup: any;
     Grid: any;
