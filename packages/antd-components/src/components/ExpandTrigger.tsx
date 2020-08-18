a64952c52bd2f84c96aec2ce1f0d5115fe0ea5e9
quirkyshop <quirkyvar@gmail.com>
2020-08-18 11:45:54 +0800

    chore(publish): v1.0.29

--- packages/antd-components/src/components/ExpandTrigger.tsx
+++ packages/antd-components/src/components/ExpandTrigger.tsx
@@ -1,28 +1,32 @@
 import React from 'react'
 import { ExpandTrigger } from '@alist/react'
-import { createVirtualBox, createControllerBox } from '@formily/antd'
+import { createVirtualBox, createControllerBox, FormSpy } from '@formily/antd'
 import { Button } from 'antd'
 
 const InternalExpandTrigger = (props) => {
-    const { render, expandText, unExpandText, form, ...others } = props
+    const { render, expandText, unExpandText, ...others } = props
 
-    return <ExpandTrigger form={form}>
-        {({ toggleExpandStatus, expandStatus }) => {
-            if (typeof render === 'function') {
-                return render({ toggleExpandStatus, expandStatus })
-            }
-            return <Button {...others} onClick={toggleExpandStatus}>
-                {expandStatus === 'expand' ?  unExpandText : expandText}
-            </Button>
-        }}
-    </ExpandTrigger>
+    return <FormSpy selector={[]}>
+      {({ form }) => {
+          return <ExpandTrigger form={form}>
+            {({ toggleExpandStatus, expandStatus }) => {
+                if (typeof render === 'function') {
+                    return render({ toggleExpandStatus, expandStatus })
+                }
+                return <Button {...others} onClick={toggleExpandStatus}>
+                    {expandStatus === 'expand' ?  unExpandText : expandText}
+                </Button>
+            }}
+        </ExpandTrigger>
+      }}
+    </FormSpy>
 }
 
 createVirtualBox('filter-expand-trigger', InternalExpandTrigger)
 const ListExpandTrigger = createControllerBox('alist-expand-trigger', (props) => {
-    const { form, schema } = props
+    const { schema } = props
     const componentProps = schema.getExtendsComponentProps()
-    return <InternalExpandTrigger {...componentProps} form={form} />
+    return <InternalExpandTrigger {...componentProps} />
 })
 
 export {
--- packages/next-components/src/components/ExpandTrigger.tsx
+++ packages/next-components/src/components/ExpandTrigger.tsx
@@ -1,28 +1,32 @@
 import React from 'react'
 import { ExpandTrigger } from '@alist/react'
-import { createVirtualBox, createControllerBox } from '@formily/next'
+import { createVirtualBox, createControllerBox, FormSpy } from '@formily/next'
 import { Button } from '@alifd/next'
 
 const InternalExpandTrigger = (props) => {
-    const { render, expandText, unExpandText, form, ...others } = props
+    const { render, expandText, unExpandText, ...others } = props
 
-    return <ExpandTrigger form={form}>
-        {({ toggleExpandStatus, expandStatus }) => {
-            if (typeof render === 'function') {
-                return render({ toggleExpandStatus, expandStatus })
-            }
-            return <Button {...others} onClick={toggleExpandStatus}>
-                {expandStatus === 'expand' ?  unExpandText : expandText}
-            </Button>
-        }}
-    </ExpandTrigger>
+    return <FormSpy selector={[]}>
+      {({ form }) => {
+        return <ExpandTrigger form={form}>
+            {({ toggleExpandStatus, expandStatus }) => {
+                if (typeof render === 'function') {
+                    return render({ toggleExpandStatus, expandStatus })
+                }
+                return <Button {...others} onClick={toggleExpandStatus}>
+                    {expandStatus === 'expand' ?  unExpandText : expandText}
+                </Button>
+            }}
+        </ExpandTrigger>
+      }}
+    </FormSpy>
 }
 
 createVirtualBox('filter-expand-trigger', InternalExpandTrigger)
 const ListExpandTrigger = createControllerBox('alist-expand-trigger', (props) => {
-    const { form, schema } = props
+    const { schema } = props
     const componentProps = schema.getExtendsComponentProps()
-    return <InternalExpandTrigger {...componentProps} form={form} />
+    return <InternalExpandTrigger {...componentProps} />
 })
 
 export {
